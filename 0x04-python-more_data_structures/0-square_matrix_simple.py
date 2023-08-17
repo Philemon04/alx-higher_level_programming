7eeeefb472539ff1077a408317f886d5d3e9aea8
Philemon04 <sjaden093@gmail.com>
2023-08-17 07:23:44 +0000

    Fxing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     matrix_len = len(matrix)
     for i in range(matrix_len):
-        second_len = len.(matrix[i])
+        second_len = len(matrix[i])
         for j in range(second_len):
             new_matrix[i][j] = (matrix[i][j]) * (matrix[i][j])
     return new_matrix
