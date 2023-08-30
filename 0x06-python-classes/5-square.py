38865fe88cc4b2eb1c7865bb208e248e11b228b9
Phil <83282650+Philemon04@users.noreply.github.com>
2023-08-30 10:50:07 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
+""" A class that defines a square by its size
+"""
 
-        self.__size = size
+
+class Square:
+    """ Method to initialize the square object
+        """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
 
     @property
     def size(self):
-        return(self.__size)
+        """ Method to returns the size value
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        try:
-            type(value) = int
-        except TypeError:
-            print("size must be an integer")
-        if value < 0:
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return(self.__size * self.__size)
+        else:
+            self.__size = value
 
     def my_print(self):
-        if size = 0:
-            print("")
-        for i in range(0, self.__size):
-            [print("#:", end="") for j in range(self.__size)]
-            print("")
+        """ Method that prints a # square according
+        to the size value
+        """
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
