df245c326e2ecf0aaa7079ec7f496e124f20bfa9
Phil <83282650+Philemon04@users.noreply.github.com>
2023-08-30 10:37:32 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,10 @@ class Square:
     def __init__(self, size=0):
         """private variable instance size"""
         try:
+        """private variable instance size"""
             type(size) = int
         except TypeError:
-            """private variable instance size"""
+        """private variable instance size"""
             print("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
